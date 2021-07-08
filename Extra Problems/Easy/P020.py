2ff7dec9683f672f1e7bdbc54fca386c19ecbefe
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2021-07-08 15:49:27 +0530

    Update P020.py

--- Extra Problems/Easy/P020.py
+++ Extra Problems/Easy/P020.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jul  4 19:17:09 2021
-
-@author: GEC DAHOD
-"""
 #Create an array with 5 values & delete the value at index no. 2
 #without using in-built function. 
 from array import *
 arr = array('i',[1,12,13,1,321,33,21,14])
 c = 0
-for i in arr:
-    if(c == 2):
-        arr.pop(x)
+for i in range(len(arr)):
+    if(i == 2):
+        arr.pop(i)
         break
 print(arr)
