6e7f97033b6d4721dc89ddac085438d8a65b9a89
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2021-07-08 12:41:43 +0530

    Update P006.py

--- Extra Problems/Easy/P006.py
+++ Extra Problems/Easy/P006.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul  3 16:51:57 2021
-
-@author: GEC DAHOD
-"""
 #Python Program for simple interest
 
 p = int(input("Enter the amount of principle: "))
