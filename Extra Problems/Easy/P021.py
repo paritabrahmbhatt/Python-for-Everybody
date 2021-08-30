7d7d0426d29148ba54827ee23c5447aa18ee78f4
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2021-08-30 18:31:18 +0530

    Update P021.py

--- Extra Problems/Easy/P021.py
+++ Extra Problems/Easy/P021.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  8 12:09:12 2021
-
-@author: GEC DAHOD
-"""
 #Program to print ASCII Value of a character
 i = input("Enter: ")
 print("ASCII value of ",i,"is: ",ord(i))
